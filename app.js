397f8430a0dc9a8e719dc2207ac33a50c78e112b
Sean Conroy <seanconroy2001@gmail.com>
2022-05-21 22:28:33 +0100

    Create basic task application

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 require("dotenv").config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const date = require("./date")
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 const app = express();
 
+const habits = [];
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    const day = date();
+    res.render("list", {date: day, habits});
+})
+
+app.post('/', (req, res) => {
+    habits.push(req.body.newHabit);
+    res.redirect("/");
 })
 
 app.listen(port, () => {
